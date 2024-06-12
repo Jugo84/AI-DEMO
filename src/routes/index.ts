d8b9a702fad18bc6ee4964c174d5943c005e6a20
Jugo84 <56642419+Jugo84@users.noreply.github.com>
2024-06-12 21:04:32 +0200

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import fs from "fs";
+import pdfjs from "pdfjs";
 
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
@@ -10,11 +11,11 @@ const apiRouter = Router();
 // @eslint-ignore-next-line
 apiRouter.get("/", async (req, res) => {
   // 83 pages
-  const bema = new PDFLoader("documents/bema.pdf", {
+  const bema = new PDFLoader("documents/KZBV_BEMA-2023-07-01.pdf", {
     splitPages: false,
-  })
-  const pdf = await bema.load()
-  res.send(pdf.map((page) => page.pageContent))
-})
+  });
+  const pdf = await bema.load();
+  res.send(pdf.map((page) => page.pageContent));
+});
 
 export default apiRouter;
