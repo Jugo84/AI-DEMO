9d1f720a8e7b14d845b9177592431a1a21af9c6d
Jugo84 <56642419+Jugo84@users.noreply.github.com>
2024-06-12 20:46:53 +0200

    wip

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
 import fs from "fs";
 
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
 // **** Variables **** //
 
 const apiRouter = Router();
 
+// @eslint-ignore-next-line
 apiRouter.get("/", async (req, res) => {
-  const bemaFile = fs.readFileSync("./documents/bema.pdf", "utf8");
-  const goäFile = fs.readFileSync("./documents/goä.pdf", "utf8");
-  const gozFile = fs.readFileSync("./documents/goz.pdf", "utf8");
+  const bema = new PDFLoader("documents/bema.pdf", {
+    splitPages: false,
+  });
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
 
   res.send("Hello World!");
 });
