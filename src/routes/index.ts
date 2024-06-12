018177f4f20a61467f193b19a100a515b992e044
Jugo84 <56642419+Jugo84@users.noreply.github.com>
2024-06-12 21:43:04 +0200

    schema

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
-import fs from "fs";
-import pdfjs from "pdfjs";
+import z from "zod";
 
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
@@ -16,5 +15,32 @@ apiRouter.get("/", async (req, res) => {
   const pdf = await bema.load();
   res.send(pdf.map((page) => page.pageContent));
 });
+
+const treatmentSchema = z.object({
+  title: z.string({ description: "Name of the treatment" }),
+  treatmentNumber: z.string({ description: "Number or id of the treatment" }),
+  description: z
+    .string({ description: "Description of the treatment" })
+    .optional()
+    .nullable(),
+  combinableTreatmentNumbers: z
+    .string({
+      description:
+        "Treatment number or ids of the potential combineable treatments",
+    })
+    .array(),
+  nonCombinableTreatmentNumbers: z
+    .string({
+      description: "Treatment number or ids of the non-combineable treatments",
+    })
+    .array(),
+});
+
+const categorySchema = z.object({
+  treatments: z.array(treatmentSchema),
+});
+const responseSchema = z.array(categorySchema);
+
+//const initialParser = StructuredOutputParser.fromZodSchema(responseSchema);
 
 export default apiRouter;
